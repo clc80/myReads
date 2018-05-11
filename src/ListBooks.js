97cb29a08132cea12373daaee6f0da597f1c0e48
clc80 <clc_80@yahoo.com>
2018-05-10 17:58:58 -0700

    Added React Router to app.js and to Listbooks.js

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
 
  class BooksList extends Component {
@@ -160,7 +161,9 @@ import React, { Component } from 'react';
                </div>
              </div>
              <div className="open-search">
-               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+               <Link
+                 to="/search"
+                 >Add a book </Link>
              </div>
            </div>
          );
